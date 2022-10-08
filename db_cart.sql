57b0ff0983a036326c03771e2c77129e979878de
Joel Viviant <joelviviant31@gmail.com>
2022-10-08 18:12:16 -0300

    database

--- db_cart.sql
+++ db_cart.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 08-10-2022 a las 06:21:22
--- Versión del servidor: 10.4.25-MariaDB
--- Versión de PHP: 8.1.10
+-- Tiempo de generación: 08-10-2022 a las 23:11:38
+-- Versión del servidor: 10.4.24-MariaDB
+-- Versión de PHP: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -54,18 +54,20 @@ CREATE TABLE `producto` (
   `nombre` varchar(255) NOT NULL,
   `categoria` int(11) NOT NULL,
   `cantidad` int(11) NOT NULL,
-  `marca` varchar(45) NOT NULL
+  `marca` varchar(45) NOT NULL,
+  `imagen` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `producto`
 --
 
-INSERT INTO `producto` (`id`, `nombre`, `categoria`, `cantidad`, `marca`) VALUES
-(95, 'carne', 3, 5, 'El paisano'),
-(96, 'detergente', 15, 25, 'Magistral'),
-(103, 'azucar', 2, 23, 'Ledesma'),
-(104, 'fideos', 2, 49, 'Matarazzo');
+INSERT INTO `producto` (`id`, `nombre`, `categoria`, `cantidad`, `marca`, `imagen`) VALUES
+(95, 'carne', 3, 5333, 'El paisano', 'Uploads/carne.jpg'),
+(96, 'detergente', 15, 25, 'Magistral', 'Uploads/detergente.jpg'),
+(103, 'azucar', 2, 23, 'Ledesma', 'Uploads/azucar.jpg'),
+(104, 'fideos', 2, 49, 'Matarazzo', 'Uploads/fideos.png'),
+(109, 'Limpiador de Piso', 15, 87, 'Poet', 'Uploads/poet.png');
 
 -- --------------------------------------------------------
 
@@ -119,13 +121,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de la tabla `categoria`
 --
 ALTER TABLE `categoria`
-  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT de la tabla `producto`
 --
 ALTER TABLE `producto`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=108;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=135;
 
 --
 -- AUTO_INCREMENT de la tabla `users`
